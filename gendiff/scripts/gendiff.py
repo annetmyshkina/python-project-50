d56f3f4af96525f67d1d1cc532069e64ab2d64dc
annetmyshkina <annet.obuchova@gmail.com>
2025-10-20 09:47:55 +0300

    add new work logic: print the result of the function generate_diff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,20 @@
+import json
+
+from gendiff.find_diff import generate_diff
 from gendiff.parser import parser_args
+
+
+def reader_file(filepath):
+    with open(filepath, 'r') as file:
+        return json.load(file)
+
 
 def main():
     args = parser_args()
-    print(args)
+    data1 = reader_file(args.first_file)
+    data2 = reader_file(args.second_file)
+    diff = generate_diff(data1, data2)
+    print(diff)
 
 
 if __name__ == "__main__":
