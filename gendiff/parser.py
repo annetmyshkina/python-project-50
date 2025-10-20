ca1da17566b22b3f1f30fa85814aa0330f2c9753
annetmyshkina <annet.obuchova@gmail.com>
2025-10-20 09:42:43 +0300

    add argument --format

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,9 +1,18 @@
 import argparse
 
+
 def parser_args():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument(
+        '-f',
+        '--format',
+        default='stylish',
+        choices=['stylish', 'plain', 'json'],
+        help='set format of output'
+    )
 
     return parser.parse_args()
