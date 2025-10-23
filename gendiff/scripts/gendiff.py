ddbcd23572f2c2a18750fc50cdd7a009a404a02a
annetmyshkina <annet.obuchova@gmail.com>
2025-10-23 12:53:47 +0300

    fix scripts logic: add new arguments from generate_diff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,12 @@
-import json
-
-from gendiff.find_diff import generate_diff
 from gendiff.parser import parser_args
-
-
-def reader_file(filepath):
-    with open(filepath, 'r') as file:
-        return json.load(file)
+from gendiff.generate_diff import generate_diff
 
 
 def main():
     args = parser_args()
-    data1 = reader_file(args.first_file)
-    data2 = reader_file(args.second_file)
-    diff = generate_diff(data1, data2)
+    data1 = args.first_file
+    data2 = args.second_file
+    diff = generate_diff(data1, data2, format_name='stylish')
     print(diff)
 
 
