103baba0434ccc1b1deb9e98f06ff191393a52d6
annetmyshkina <annet.obuchova@gmail.com>
2025-10-25 14:44:07 +0300

    add tests for json.py

---
+++ tests/test_json.py
@@ -0,0 +1,37 @@
+import json
+from gendiff.generate_diff import generate_diff
+
+expected_path = 'tests/test_data/result_json.txt'
+with open(expected_path, 'r', encoding="utf-8") as file:
+    expected = file.read().strip()
+
+
+def test_json_diff():
+    data1_path = 'tests/test_data/data1.json'
+    data2_path = 'tests/test_data/data2.json'
+
+    result = generate_diff(data1_path, data2_path, format_name='json')
+
+    assert isinstance(result, str)
+    assert result == expected
+    assert len(result) > 0
+    parsed = json.loads(result)
+    assert isinstance(parsed, list)
+    assert 'key' in parsed[0]
+    assert 'status' in parsed[0]
+
+
+
+def test_yml_diff():
+    data1_path = 'tests/test_data/YML_data1.yml'
+    data2_path = 'tests/test_data/YML_data2.yaml'
+
+    result = generate_diff(data1_path, data2_path, format_name='json')
+
+    assert isinstance(result, str)
+    assert result == expected
+    assert len(result) > 0
+    parsed = json.loads(result)
+    assert isinstance(parsed, list)
+    assert 'key' in parsed[0]
+    assert 'status' in parsed[0]
