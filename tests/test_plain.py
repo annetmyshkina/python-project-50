191744ad475de11df697cd94dec95e1465cd716c
annetmyshkina <annet.obuchova@gmail.com>
2025-10-25 12:10:17 +0300

    add tests for plain.py

---
+++ tests/test_plain.py
@@ -0,0 +1,23 @@
+from gendiff.generate_diff import generate_diff
+
+expected_path = 'tests/test_data/result_plain.txt'
+with open(expected_path, 'r', encoding="utf-8") as file:
+    expected = file.read().strip()
+
+
+def test_json_diff():
+    data1_path = 'tests/test_data/data1.json'
+    data2_path = 'tests/test_data/data2.json'
+
+    result = generate_diff(data1_path, data2_path, format_name='plain')
+
+    assert result == expected
+
+
+def test_yml_diff():
+    data1_path = 'tests/test_data/YML_data1.yml'
+    data2_path = 'tests/test_data/YML_data2.yaml'
+
+    result = generate_diff(data1_path, data2_path, format_name='plain')
+
+    assert result == expected
